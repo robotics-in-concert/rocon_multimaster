1164bcc0e89fd2481b003b200383cbfed274f596
Jihoon <jihoonl@whoolala.(none)>
2012-09-06 14:52:42 +0900

    moving old implementation to graveyard. and resturcture the stack

---
+++ multimaster_client/rocon_gateway_sync/example_scripts/test_local.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2012, Yujin Robot, Daniel Stonier
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Yujin Robot nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import sys
+import time
+import threading
+import roslib; roslib.load_manifest('rocon_gateway_sync')
+import rospy
+import rocon_gateway_sync
+
+#    def register_cb(self, data):
+#        print("Registering local api [" + self.uri + "] to the remote gateway")
+
+##############################################################################
+# Launching
+##############################################################################
+
+# Make sure you launch this with test_local.launch which will also launch
+# a roscpp_tutorial talker
+
+if __name__ == '__main__':
+    rospy.init_node('local_gateway_sync')
+    gateway = rocon_gateway_sync.GatewaySync()
+    rospy.spin()
+    gateway.shutdown()
---
+++ multimaster_client/rocon_gateway_sync/src/rocon_gateway_sync/local_manager.py
@@ -0,0 +1,126 @@
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2012, Yujin Robot, Daniel Stonier
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Yujin Robot nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import roslib; roslib.load_manifest('rocon_gateway_sync')
+import rosgraph
+
+class LocalManager(object):
+    '''
+    Abstraction layer over the xmlrpc interface to the local master.
+    
+    The master_sync used an entirely callback driven mechanism to 
+    handle creation and destruction of publishers and
+    subscribers. Can the rosgraph masterapi do the same?
+    '''
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.master_uri = rosgraph.get_master_uri()
+        
+        ns = rosgraph.names.get_ros_namespace() # ns in which this node resides, typically '/'
+        anon_name = rosgraph.names.anonymous_name('gateway') # creates a unique name, e,g, gateway_0258198135
+
+        self.master = rosgraph.masterapi.Master(rosgraph.names.ns_join(ns, anon_name), master_uri=self.master_uri)
+        
+        #self.cb = cb
+        
+        # Should we store these in the redis store? Probably not - it would take too long to 
+        # continually do lookups. 
+        self.type_cache = {}
+        self.subs = {}
+        self.pubs = {}
+        self.srvs = {}
+    
+    def get_topic_type(self, query_topic):
+        query_topic = self.resolve(query_topic)
+        if query_topic in self.type_cache:
+            return self.type_cache[query_topic]
+        else:
+            for topic, topic_type in self.master.getTopicTypes():
+                self.type_cache[topic] = topic_type
+            if query_topic in self.type_cache:
+                return self.type_cache[query_topic]
+            else:
+                return "*"
+            
+    def resolve(self, topic):
+        ns = rosgraph.names.namespace(self.master.caller_id)
+        return rosgraph.names.ns_join(ns, topic)
+
+    def registerSubscriber(self, topic, type, node, nodeuri):
+        '''
+        Triggered by a callback from the redis store.
+        '''
+        pass
+    
+    def registerPublisher(self, topic, type, node, nodeuri):
+        '''
+        Triggered by a callback from the redis store.
+        '''
+        pass
+
+    def registerService(self, service, serviceuri, node, nodeuri):
+        '''
+        Triggered by a callback from the redis store.
+        '''
+        pass
+
+    def unregisterPublisher(self, topic, node, nodeuri):
+        '''
+        Triggered by a callback from the redis store.
+        '''
+        pass
+
+    def unregisterSubscriber(self, topic, node, nodeuri):
+        '''
+        Triggered by a callback from the redis store.
+        '''
+        pass
+    
+    def unregisterService(self, service, serviceuri, node):
+        '''
+        Triggered by a callback from the redis store.
+        '''
+        pass
+    
+    def unsubscribeAll(self):
+        '''
+        Roll through all registrations and clean out.
+        '''
+#        for (t,u),m in self.subs.iteritems():
+#            m.unregisterSubscriber(t,u)
+#        for t,u in self.pubs.keys():
+#            self.unadvertise(t,u)
+#        for s in self.srvs.keys():
+#            self.unadvertise_service(s)
+        pass    
