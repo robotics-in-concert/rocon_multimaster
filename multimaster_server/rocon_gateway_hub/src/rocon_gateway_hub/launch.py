1905020947f18da1a1f4706a7e657f1384a7219c
Daniel Stonier <d.stonier@gmail.com>
2012-09-24 10:55:41 +0900

    slightly better handling of avahi.

--- multimaster_server/rocon_gateway_hub/src/rocon_gateway_hub/launch.py
+++ multimaster_server/rocon_gateway_hub/src/rocon_gateway_hub/launch.py
@@ -119,10 +119,13 @@ def advertise_port_to_avahi(config, hub_name):
     '''
       Check if avahi-daemon is around and publish the redis server ip:port.
     '''
-    # Check
+    # Check - assuming ubuntu here, robustify later
     proc = subprocess.Popen(["pidof","avahi-daemon"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     if proc.stdout.read() == "":
         sys.exit(utils.logfatal("hub: could not find the avahi-daemon - is it running?"))
+        sys.exit(utils.logfatal("   : if your isp is misbehaving and avahi not autostarting"))
+        sys.exit(utils.logfatal("   : you may need to set AVAHI_DAEMON_DETECT_LOCAL=0"))
+        sys.exit(utils.logfatal("   : in /etc/default/avahi-daemon"))
 
     port = config["port"]
     # if you don't specify  stdout/stderr streams, then it will automatically go to the background
