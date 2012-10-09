945eebc64f18c707c35748f804ca3b7dd5b9ddc5
Daniel Stonier <d.stonier@gmail.com>
2012-10-09 09:01:07 +0900

    flip_chatter now automagically finding a gateway to flip to.

--- multimaster_client/rocon_gateway_tests/tests/multimaster/flip_chatter.py
+++ multimaster_client/rocon_gateway_tests/tests/multimaster/flip_chatter.py
@@ -9,6 +9,7 @@ import rospy
 from gateway_comms.msg import *
 from gateway_comms.srv import *
 import argparse
+import sys
 
 """
   Tests a single flip rule.
@@ -17,30 +18,38 @@ import argparse
     1 > roslaunch rocon_gateway_hub pirate.launch
     2a> roslaunch rocon_gateway pirate_chatter.launch
     3a> roslaunch rocon_gateway pirate.launch
-    3b> rosrun rocon_gateway_tests flip_chatter gateway1
+    3b> rosrun rocon_gateway_tests flip_chatter
     2b> rostopic list
 """
 
 if __name__ == '__main__':
 
   parser = argparse.ArgumentParser(description='Flip /chatter to a remote gateway')
-  parser.add_argument("gateway", help="gateway string identifier", type=str)
+  #parser.add_argument("gateway", help="gateway string identifier", type=str)
   parser.add_argument('--cancel', action='store_true', help='cancel the flip')
   args = parser.parse_args()
 
   rospy.init_node('flip_chatter')
 
+  remote_gateway_info = rospy.ServiceProxy('/gateway/remote_gateway_info',RemoteGatewayInfo)
   flip = rospy.ServiceProxy('/gateway/flip',Flip)
   
+  req = RemoteGatewayInfoRequest()
+  req.gateways = []
+  resp = remote_gateway_info()
+  if len(resp.gateways) == 0:
+      rospy.logerr("Flip Test : no other gateways available to flip to, aborting.")
+      sys.exit(1)
+  else:
+      gateway = resp.gateways[0].name
+      
   # Form a request message
   req = FlipRequest() 
   req.flip_rule.connection.name = "/chatter"
   req.flip_rule.connection.type = gateway_comms.msg.Connection.PUBLISHER
-  req.flip_rule.gateway = args.gateway
+  req.flip_rule.gateway = gateway
   req.cancel = args.cancel
   
-  #req.node_name = "talker"
-  #req.remapped_name = "dude"
   print ""
   print "== Request =="
   print ""
