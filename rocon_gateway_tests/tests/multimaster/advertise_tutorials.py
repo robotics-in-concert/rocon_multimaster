9723fd47ab381da43c40d27124f1f38b6f7e9cee
Daniel Stonier <d.stonier@gmail.com>
2012-10-16 17:53:14 +0900

    merged last of common code from flipped and pulled interfaces, closes #90

--- rocon_gateway_tests/tests/multimaster/advertise_tutorials.py
+++ rocon_gateway_tests/tests/multimaster/advertise_tutorials.py
@@ -89,4 +89,5 @@ if __name__ == '__main__':
         req.rules.append(rule) 
         resp = advertise(req)
         if resp.result != 0:
-            rospy.logerr("Advertise : %s"%resp.error_message)
+            rospy.logerr("Advertise : error occured (todo: no error message yet)")
+            #rospy.logerr("Advertise : %s"%resp.error_message)
