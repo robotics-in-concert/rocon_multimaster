e5c683a881c336293f1058ba2bd2ecde097e2a59
Daniel Stonier <d.stonier@gmail.com>
2012-11-27 00:19:06 +0900

    script almost where I want it, for unflips need to parse watchlist though, not flips.

--- rocon_gateway/src/rocon_gateway/console.py
+++ rocon_gateway/src/rocon_gateway/console.py
@@ -33,11 +33,11 @@ reset = "\x1b[0m"
 
 bold = '1'
 black, red, green, yellow, blue, magenta, cyan, white = [str(i) for i in range(30,38)]
-bold_black, bold_red, bold_gree, bold_yellow, bold_blue, bold_magenta, bold_cyan, bold_white = [ '1;'+ str(i) for i in range(30,38)]
+bold_black, bold_red, bold_green, bold_yellow, bold_blue, bold_magenta, bold_cyan, bold_white = [ '1;'+ str(i) for i in range(30,38)]
 colours = [ 
            bold,
            black, red, green, yellow, blue, magenta, cyan, white,
-           bold_black, bold_red, bold_gree, bold_yellow, bold_blue, bold_magenta, bold_cyan, bold_white 
+           bold_black, bold_red, bold_green, bold_yellow, bold_blue, bold_magenta, bold_cyan, bold_white 
           ] 
 
 def pretty_print(msg, colour=white):
