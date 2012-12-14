fb39ac87662714053e690f154a61cbd4bde473f6
Daniel Stonier <d.stonier@gmail.com>
2012-12-14 13:17:44 +0900

    pep8 stuff.

--- rocon_hub_client/src/rocon_hub_client/hub_client.py
+++ rocon_hub_client/src/rocon_hub_client/hub_client.py
@@ -3,33 +3,33 @@
 # License: BSD
 #   https://raw.github.com/robotics-in-concert/rocon_multimaster/master/rocon_hub_client/LICENSE
 #
-
 ##############################################################################
 # Imports
 ##############################################################################
 
 import rospy
-import threading
-import roslib; roslib.load_manifest('rocon_hub_client')
+import roslib
+roslib.load_manifest('rocon_hub_client')
 import redis
 from .hub_connector import HubConnector
 
+
 class HubClient(HubConnector):
 
-    # HubConnector provide 
+    # HubConnector provide
     #    connect(hub_uri)
-    
+
     ConnectionError = redis.exceptions.ConnectionError
 
     pool = None
     server = None
-    pubsub= None
+    pubsub = None
     callback = None
-    namespace= ''
+    namespace = ''
     param = {}
 
     def __init__(self, whitelist, blacklist, is_zeroconf, namespace, name, callbacks):
-        super(HubClient,self).__init__(whitelist, blacklist, is_zeroconf, self._connectToHub)
+        super(HubClient, self).__init__(whitelist, blacklist, is_zeroconf, self._connectToHub)
 
         self._namespace = namespace
         self._name = name
@@ -39,43 +39,41 @@ class HubClient(HubConnector):
         self._redis_keys = {}
 #        self._redis_keys['index'] = createHubKey('index')
         self._redis_channels = {}
-        self._redis_pubsub_server= None
+        self._redis_pubsub_server = None
 
-    def _connectToHub(self,ip,portarg):
+    def _connectToHub(self, ip, portarg):
         try:
-            self.pool = redis.ConnectionPool(host=ip,port=portarg,db=0)
+            self.pool = redis.ConnectionPool(host=ip, port=portarg, db=0)
             self.server = redis.Redis(connection_pool=self.pool)
             rospy.logdebug("HubClient : connected to the hub's redist server.")
             self._redis_pubsub_server = self.server.pubsub()
-        except redis.exceptions.ConnectionError as e:
+        except redis.exceptions.ConnectionError as unused_e:
             rospy.logerror("HubClient : failed rule to the hub's redis server.")
             return False
             raise
         return True
 
-    def registerKey(self,key,element):
-        key = self._namespace + ":"+ key
+    def registerKey(self, key, element):
+        key = self._namespace + ":" + key
         element = self._namespace + ":" + element
-        
-        if self.server.sadd(key,element):
+
+        if self.server.sadd(key, element):
             return True
         else:
             raise Exception("Same concertmaster name exist")
             return False
 
-    def unregisterKey(self,key,element):
+    def unregisterKey(self, key, element):
         try:
-            key = self._namespace + ":"+ key
+            key = self._namespace + ":" + key
             element = self._namespace + ":" + element
-            self.server.srem(key,element)
+            self.server.srem(key, element)
         except redis.exceptions.ConnectionError:
             raise  # ~HubClient.ConnectionError
 
-
-    def getValues(self,key):
-        key = self._namespace + ":"+ key
+    def getValues(self, key):
+        key = self._namespace + ":" + key
         values = self.server.smembers(key)
 
-        values = [ v[len(self._namespace)+1:] for v in values]
+        values = [v[len(self._namespace) + 1:] for v in values]
         return values
-
--- rocon_hub_client/src/rocon_hub_client/hub_connector.py
+++ rocon_hub_client/src/rocon_hub_client/hub_connector.py
@@ -11,19 +11,23 @@
 import rospy
 import rocon_gateway
 import redis
-from urlparse import urlparse
 
 import zeroconf_msgs.srv
-from gateway_msgs.msg import *
+import gateway_msgs.msg as gateway_msgs
+
+##############################################################################
+# HubConnector
+##############################################################################
+
 
 class HubConnector(object):
     hub_service = "_ros-multimaster-hub._tcp"
     is_connected = False
 
-    def __init__(self,whitelist,blacklist,is_zeroconf,connectFunc):
+    def __init__(self, whitelist, blacklist, is_zeroconf, connectFunc):
 
         self.param = {} if self.param == None else self.param
-            
+
         self.param['whitelist'] = whitelist
         self.param['blacklist'] = blacklist
         self.connectToHub = connectFunc
@@ -32,26 +36,25 @@ class HubConnector(object):
         if is_zeroconf:
             self._zeroconf_services = rocon_gateway.zeroconf.setupRosServices()
 
-    def connect(self,hub_uri=None):
+    def connect(self, hub_uri=None):
         if hub_uri:
             return self.direct_connect(hub_uri)
         else:
             return self.zeroconfConnect()
 
-    def direct_connect(self,hub_uri):
+    def direct_connect(self, hub_uri):
         '''
             If hub_uri is provided, attempt a direct connection
         '''
         (ip, port) = hub_uri.split(':')
 
-        if self._connect(ip, int(port)) == gateway_msgs.msg.Result.SUCCESS:
-            rospy.loginfo("HubConnector : made direct connection to the hub [%s]"%hub_uri)
+        if self._connect(ip, int(port)) == gateway_msgs.Result.SUCCESS:
+            rospy.loginfo("HubConnector : made direct connection to the hub [%s]" % hub_uri)
             self.is_connected = True
             return True
         else:
-            rospy.logwarn("HubConnector : failed direct connection attempt to hub [%s]"%hub_uri)
+            rospy.logwarn("HubConnector : failed direct connection attempt to hub [%s]" % hub_uri)
             return False
-
 
     def zeroconf_connect(self):
         while not rospy.is_shutdown() and not self.is_connected:
@@ -60,21 +63,20 @@ class HubConnector(object):
                 found_hubs = self._scan_for_zeroconf_hubs()
                 for (ip, port) in found_hubs:
                     rospy.loginfo("HubConnector : discovered hub zeroconf service at " + str(ip) + ":" + str(port))
-                    if self._connect(ip,port) == gateway_msgs.msg.Result.SUCCESS:
+                    if self._connect(ip, port) == gateway_msgs.Result.SUCCESS:
                         self.is_connected = True
                         break
                 rospy.logdebug("HubConnector : waiting for hub uri input")
 
         return self.is_connected
 
-
     def _scan_for_zeroconf_hubs(self):
-        ''' 
+        '''
             Does a quick scan on zeroconf for gateway hubs. If new ones are
             found, and it is not on the blacklist, it attempts a connection.
-                                            
+
             This gets run in the pre-spin part of the spin loop.
-                                                        
+
             @param previously_found_hubs: zeroconf names of previously scanned hubs
             @type  previously_found_hubs: list of str
         '''
@@ -82,11 +84,11 @@ class HubConnector(object):
         previously_found_hubs = []
         found_hubs = []
         # Get discovered redis server list from zeroconf
-        req = zeroconf_msgs.srv.ListDiscoveredServicesRequest() 
+        req = zeroconf_msgs.srv.ListDiscoveredServicesRequest()
         req.service_type = rocon_gateway.zeroconf.gateway_hub_service
         resp = self._zeroconf_services["list_discovered_services"](req)
         rospy.logdebug("HubConnector : checking for autodiscovered gateway hubs")
-        new_services = lambda l1,l2: [x for x in l1 if x not in l2] 
+        new_services = lambda l1,l2: [x for x in l1 if x not in l2]
                                         
         for service in new_services(resp.services,previously_found_hubs):
             (ip, port) = rocon_gateway.zeroconf.resolve_address(service)
@@ -97,25 +99,25 @@ class HubConnector(object):
     def _connect(self,ip,port):
         if self.is_connected: 
             rospy.logwarn("HubConnector : gateway is already connected, aborting connection attempt.")
-            return gateway_msgs.msg.Result.HUB_CONNECTION_ALREADY_EXISTS
+            return gateway_msgs.Result.HUB_CONNECTION_ALREADY_EXISTS
         try:
             hub_name = rocon_gateway.resolve_hub(ip,port)
             rospy.loginfo("HubConnector : resolved hub name [%s].", hub_name)
         except redis.exceptions.ConnectionError:
             rospy.logerr("HubConnector : couldn't connect to the hub [%s:%s]", ip, port)
-            return gateway_msgs.msg.Result.HUB_CONNECTION_UNRESOLVABLE
+            return gateway_msgs.Result.HUB_CONNECTION_UNRESOLVABLE
         if ip in self.param['blacklist']:
             rospy.loginfo("HubConnector : ignoring blacklisted hub [%s]",ip)
-            return gateway_msgs.msg.Result.HUB_CONNECTION_BLACKLISTED
+            return gateway_msgs.Result.HUB_CONNECTION_BLACKLISTED
         elif hub_name in self.param['blacklist']:
             rospy.loginfo("HubConnector : ignoring blacklisted hub [%s]",hub_name)
-            return gateway_msgs.msg.Result.HUB_CONNECTION_BLACKLISTED
+            return gateway_msgs.Result.HUB_CONNECTION_BLACKLISTED
         # Handle whitelist (ip or hub name)
         if (len(self.param['whitelist']) == 0) or (ip in self.param['whitelist']) or (hub_name in self.param['whitelist']):
             if self.connectToHub(ip,port):
-                return gateway_msgs.msg.Result.SUCCESS
+                return gateway_msgs.Result.SUCCESS
             else:
-                return gateway_msgs.msg.Result.HUB_CONNECTION_FAILED
+                return gateway_msgs.Result.HUB_CONNECTION_FAILED
         else:
             rospy.loginfo("HubConnector : hub/ip not in non-empty whitelist [%s]",hub_name)
-            return gateway_msgs.msg.Result.HUB_CONNECTION_NOT_IN_NONEMPTY_WHITELIST
+            return gateway_msgs.Result.HUB_CONNECTION_NOT_IN_NONEMPTY_WHITELIST
