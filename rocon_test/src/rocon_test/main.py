2f19421753c50e652c57a0990ea3f4c06e92bd97
Daniel Stonier <d.stonier@gmail.com>
2013-04-04 18:49:18 +0900

    basically rocon-test = rostest.

--- rocon_test/src/rocon_test/main.py
+++ rocon_test/src/rocon_test/main.py
@@ -10,12 +10,18 @@
 
 from __future__ import print_function
 
+import os
+import sys
+import unittest
+import rostest.runner
 import roslaunch.rlutil
 import roslaunch.core
 import logging
 import rospkg
 import argparse
 from argparse import RawTextHelpFormatter
+from rostest.rostestutil import printRostestSummary, xmlResultsFile, \
+                                createXMLRunner, rostest_name_from_path
 
 ##############################################################################
 # Methods
@@ -48,8 +54,9 @@ def _parse_arguments():
 #    parser.add_argument('--make-args', dest='make_args', nargs='*', type=str,
 #        help='Arbitrary arguments which are passes to make. It must be passed after other arguments since it collects all following options. This is only necessary in combination with --cmake-args since else all unknown arguments are passed to make anyway.')
     args = parser.parse_args()
+    # Stop it from being a list (happens when nargs is an integer)
+    args.test = args.test[0]
     #(options, args) = parser.parse_args()
-    print("Args...........%s"%args) 
 #    try:
 #        args = roslaunch.rlutil.resolve_launch_arguments(args)
 #    except roslaunch.core.RLException as e:
@@ -63,10 +70,49 @@ def test_main():
     logger = logging.getLogger('rocon_test')
     roslaunch.core.add_printlog_handler(logger.info)
     roslaunch.core.add_printerrlog_handler(logger.error)
-    pkg = rospkg.get_package_name(args.filename)
-    rospack = rospkg.RosPack()
-    pkg_dir = rospack.get_path(pkg)
-    print("Pkg...........%s"%pkg)
-    print("Pkg Dir...........%s"%pkg_dir)
-    print("Filename..........%s"%args.filename)
+    if not args.package:
+        if not os.path.isfile(args.test):
+            raise RuntimeError("Test launcher file does not exist [%s]." % args.test)
+        else:
+            args.package = rospkg.get_package_name(args.test)
+    r = rospkg.RosPack()
+    pkg_dir = r.get_path(args.package)
+    outname = rostest_name_from_path(pkg_dir, args.test)  # underscored pkg_dir relative name (e.g. launch/pirate_chatter.multilaunch -> launch_pirate_chatter
+    try:
+        test_case = rostest.runner.createUnitTest(args.package, args.test)
+        suite = unittest.TestLoader().loadTestsFromTestCase(test_case)
 
+        is_rostest = True
+        results_file = xmlResultsFile(args.package, outname, is_rostest)        
+        xml_runner = createXMLRunner(args.package, outname, \
+                                         results_file=results_file, \
+                                         is_rostest=is_rostest)
+        result = xml_runner.run(suite)
+    finally:
+        # really make sure that all of our processes have been killed
+        test_parents = rostest.runner.getRostestParents()
+        for r in test_parents:
+            logger.info("finally rostest parent tearDown [%s]", r)
+            r.tearDown()
+        del test_parents[:]
+        from roslaunch.pmon import pmon_shutdown
+        logger.info("calling pmon_shutdown")
+        pmon_shutdown()
+        logger.info("... done calling pmon_shutdown")
+    # print config errors after test has run so that we don't get caught up in .xml results
+    config = rostest.runner.getConfig()
+    if config:
+        if config.config_errors:
+            print("\n[ROSTEST WARNINGS]"+'-'*62+'\n', file=sys.stderr)
+        for err in config.config_errors:
+            print(" * %s"%err, file=sys.stderr)
+        print('')
+
+    # summary is worthless if textMode is on as we cannot scrape .xml results
+    subtest_results = rostest.runner.getResults()
+    printRostestSummary(result, subtest_results)
+
+    if not result.wasSuccessful():
+        sys.exit(1)
+    elif subtest_results.num_errors or subtest_results.num_failures:
+        sys.exit(2)
