178b76a18f000c1992863e377f93990f77b9c388
Daniel Stonier <d.stonier@gmail.com>
2013-04-07 14:35:42 +0900

    first gateway test working - tests a graph update.

--- rocon_gateway_tests/scripts/graph.py
+++ rocon_gateway_tests/scripts/graph.py
@@ -24,18 +24,27 @@ class TestGraph(unittest.TestCase):
     def setUp(self):
         rospy.init_node('test_graph')
         self.graph = Graph()
-        rospy.sleep(1.0)
-        self.graph.update()
 
     def test_graph(self):
+        flips = None
+        while not flips:
+            rospy.loginfo("Got flips")
+            self.graph.update()
+            flips = self.graph._local_gateway.flip_watchlist
+            rospy.sleep(0.2)
         print("********************************************************************")
         print("* Local Gateway")
         print("********************************************************************")
-        print self.graph._local_gateway
+        print("%s" % self.graph._local_gateway)
+        self.assertEquals("1", str(len(flips)))
+        self.assertEquals("remote_gateway", flips[0].gateway)
+        self.assertEquals("publisher", flips[0].rule.type)
+        self.assertEquals("/chatter", flips[0].rule.name)
+        
         print("********************************************************************")
         print("* Remote Gateway")
         print("********************************************************************")
-        print self.graph._remote_gateways
+        print("%s" % self.graph._remote_gateways)
         for remote_gateway in self.graph._remote_gateways:
             self.assertEquals("remote_gateway", remote_gateway.name)
 
@@ -45,5 +54,4 @@ class TestGraph(unittest.TestCase):
 NAME = 'test_graph'
 if __name__ == '__main__':
     rosunit.unitrun('test_graph', NAME, TestGraph, sys.argv, coverage_packages=['rocon_gateway'])
-
         
