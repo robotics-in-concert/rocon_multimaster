2882f51f718ce3aac66832f971d8017164cf6f92
Daniel Stonier <d.stonier@gmail.com>
2013-04-20 18:38:00 +0900

    hub white/blacklist checks for hubs.

--- rocon_hub_client/src/rocon_hub_client/hub_client.py
+++ rocon_hub_client/src/rocon_hub_client/hub_client.py
@@ -10,6 +10,7 @@
 import redis
 import rospy
 from urlparse import urlparse
+import rocon_utilities
 
 # local imports
 import hub_api
@@ -56,10 +57,14 @@ class Hub(object):
             self._redis_server = None
             raise HubNotFoundError("couldn't connect to the redis server")
 
-        # whitelists, blacklists
+        # whitelists, blacklists - check against uri's hash names and non-uuid names
         uri_blacklist = [urlparse(x).hostname + ':' + str(urlparse(x).port) for x in blacklist if urlparse(x).hostname is not None]
         uri_whitelist = [urlparse(x).hostname + ':' + str(urlparse(x).port) for x in whitelist if urlparse(x).hostname is not None]
-        if self.uri in uri_blacklist or self.name in blacklist:
+        nonuuid_blacklist = [rocon_utilities.gateway_basename(x) for x in blacklist if urlparse(x) is None and rocon_utilities.gateway_basename(x)]
+        nonuuid_whitelist = [rocon_utilities.gateway_basename(x) for x in whitelist if urlparse(x) is None and rocon_utilities.gateway_basename(x)]
+        if self.uri in uri_blacklist or self.name in blacklist or self.name in nonuuid_blacklist:
+            raise HubConnectionBlacklistedError("ignoring blacklisted hub [%s]" % self.uri)
+        if self.name in blacklist or self.name in nonuuid_whitelist:
             raise HubConnectionBlacklistedError("ignoring blacklisted hub [%s]" % self.uri)
         if not ((len(whitelist) == 0) or (self.uri in uri_whitelist) or (self.name in whitelist)):
             raise HubConnectionNotWhitelistedError("hub/ip not in non-empty whitelist [%s][%s]%s" % (self.name, self.uri, whitelist))
