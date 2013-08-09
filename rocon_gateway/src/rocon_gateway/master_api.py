6cd6ff6df9a942a2c56bb9144668083ec9c08317
Daniel Stonier <d.stonier@gmail.com>
2013-08-09 09:39:26 +0900

    error checking for master api register service bug.

--- rocon_gateway/src/rocon_gateway/master_api.py
+++ rocon_gateway/src/rocon_gateway/master_api.py
@@ -333,6 +333,12 @@ class LocalMaster(rosgraph.Master):
                 rospy.logwarn("Gateway : tried to register a service that is already locally available, aborting [%s]" % registration.connection.rule.name)
                 return None
             else:
+                if registration.connection.rule.name is None:
+                    rospy.logerr("Gateway : tried to register a service with name set to None")
+                if registration.connection.type_info is None:
+                    rospy.logerr("Gateway : tried to register a service with type_info set to None")
+                if registration.connection.xmlrpc_uri is None:
+                    rospy.logerr("Gateway : tried to register a service with xmlrpc_uri set to None")
                 node_master.registerService(registration.connection.rule.name, registration.connection.type_info, registration.connection.xmlrpc_uri)
                 return registration
         elif registration.connection.rule.type == ACTION_SERVER:
