576e72376bca840f8d1af51d90c7267d7c655633
Daniel Stonier <d.stonier@gmail.com>
2013-11-03 23:03:26 +0900

    export find_resource_from_string

--- rocon_utilities/src/rocon_utilities/__init__.py
+++ rocon_utilities/src/rocon_utilities/__init__.py
@@ -17,7 +17,7 @@ import console
 from launch import main as launch, parse_rocon_launcher
 from gateways import create_gateway_remote_rule, create_gateway_rule, gateway_basename
 import ros_utilities as ros
-from ros_utilities import SubscriberProxy, find_resource
+from ros_utilities import SubscriberProxy, find_resource, find_resource_from_string
 from exceptions import TimeoutExpiredError
 from icons import icon_to_msg, icon_resource_to_msg
 from wall_rate import WallRate
