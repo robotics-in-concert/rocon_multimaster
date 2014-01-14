49e06260c444373a6659554e98a4f1afa6aa2478
Daniel Stonier <d.stonier@gmail.com>
2014-01-14 17:39:54 +0900

    deprecated removal of SubscriberProxy, closes #243.

--- rocon_utilities/src/rocon_utilities/__init__.py
+++ rocon_utilities/src/rocon_utilities/__init__.py
@@ -2,13 +2,15 @@
 # License: BSD
 #   https://raw.github.com/robotics-in-concert/rocon_multimaster/master/rocon_utilities/LICENSE
 #
+##############################################################################
+# Imports
+##############################################################################
 
 import console
 from launch import main as launch, parse_rocon_launcher
 from gateways import create_gateway_remote_rule, create_gateway_rule, gateway_basename
 import ros_utilities as ros
 from ros_utilities import (
-        SubscriberProxy,
         find_resource,
         find_resource_from_string,
         package_index_from_package_path
@@ -19,3 +21,15 @@ from wall_rate import WallRate
 from system import Popen
 from pinger import Pinger
 import platform_info
+
+##############################################################################
+# Deprecating Messages
+##############################################################################
+# SubscriberProxy has moved, but bring it in here to support current code
+# and notify users of a new repository.
+try:
+    from rocon_python_comms import SubscriberProxy
+except ImportError:
+    import sys
+    console.logerror("Some functionality has shifted - please make sure [rocon_tools](https://github.com/robotics-in-concert/rocon_tools) is in your workspace.")
+    sys.exit(1)
