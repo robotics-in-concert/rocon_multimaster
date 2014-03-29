d2617c0920524eadd27728051d5fc397399b18d2
Daniel Stonier <d.stonier@gmail.com>
2014-03-29 20:36:53 +0900

    rocon_launch moved to rocon_tools

--- rocon_launch/src/rocon_launch/__init__.py
+++
@@ -1,9 +0,0 @@
-#
-# License: BSD
-#   https://raw.github.com/robotics-in-concert/rocon_multimaster/license/LICENSE
-#
-##############################################################################
-# Imports
-##############################################################################
-
-from launch import main as launch, parse_rocon_launcher
